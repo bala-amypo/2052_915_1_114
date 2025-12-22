c3f964fb40fcd81b70baa8d365f823d4c3b853d8
bala-amypo <bala@amypo.in>
2025-12-22 08:53:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/ColdRoom.java
+++ src/main/java/com/example/demo/entity/ColdRoom.java
@@ -1,70 +1,29 @@
-package com.example.demo.entity;
+package com.example.demo.controller;
 
-import jakarta.persistence.*;
+import com.example.demo.entity.ColdRoom;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 import java.util.List;
 
-@Entity
-public class ColdRoom {
+@RestController
+@RequestMapping("/coldrooms")
+public class ColdRoomController {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String roomName;
-
-    private String location;
-
-    private Double minTemperature;
-    private Double maxTemperature;
-
-    @OneToMany(mappedBy = "coldRoom", cascade = CascadeType.ALL)
-    private List<SensorDevice> sensors;
-
-    public Long getId() {
-        return id;
+    @PostMapping
+    public ColdRoom create(@RequestBody ColdRoom coldRoom) {
+        return coldRoom;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    @GetMapping
+    public List<ColdRoom> getAll() {
+        return Collections.emptyList();
     }
 
-    public String getRoomName() {
-        return roomName;
-    }
-
-    public void setRoomName(String roomName) {
-        this.roomName = roomName;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Double getMinTemperature() {
-        return minTemperature;
-    }
-
-    public void setMinTemperature(Double minTemperature) {
-        this.minTemperature = minTemperature;
-    }
-
-    public Double getMaxTemperature() {
-        return maxTemperature;
-    }
-
-    public void setMaxTemperature(Double maxTemperature) {
-        this.maxTemperature = maxTemperature;
-    }
-
-    public List<SensorDevice> getSensors() {
-        return sensors;
-    }
-
-    public void setSensors(List<SensorDevice> sensors) {
-        this.sensors = sensors;
+    @GetMapping("/{id}")
+    public ColdRoom getById(@PathVariable Long id) {
+        ColdRoom cr = new ColdRoom();
+        cr.setId(id);
+        return cr;
     }
 }
