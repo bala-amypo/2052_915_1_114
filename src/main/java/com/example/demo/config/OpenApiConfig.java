6ae4075c6d47f81dac291e541540eddae51b56eb
bala-amypo <bala@amypo.in>
2025-12-26 17:38:09 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,19 +1,46 @@
 package com.example.demo.config;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        Server productionServer = new Server()
+                .url("https://9250.pro604cr.amypo.ai")
+                .description("Production Server");
+
+        Server localServer = new Server()
+                .url("http://localhost:9001")
+                .description("Local Server");
+
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9067.408procr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("Digital Asset Lifecycle & Audit Trail API")
+                        .description("Asset management with lifecycle, transfers, disposals and JWT security")
+                        .version("1.0.0")
+                )
+                .servers(List.of(productionServer, localServer))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", bearerScheme)
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                );
+    }
 }
