890aa7cbab6691ce8783d5a21caeea4750dd85bb
bala-amypo <bala@amypo.in>
2025-12-27 05:26:48 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
-
+    
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
@@ -19,9 +19,7 @@ public class OpenApiConfig {
                         .version("1.0")
                         .description("Demo Spring Boot API"))
                 .servers(List.of(
-                        new Server()
-                                .url("https://9067.408procr.amypo.ai")
-                                .description("Remote deployed server")
+                        new Server().url("https://9067.408procr.amypo.ai/")
                 ));
     }
 }
